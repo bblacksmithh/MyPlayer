4607e6a29999fa7ac014679ae98c7934106f536b
bblacksmithh <101953303+bblacksmithh@users.noreply.github.com>
2023-11-26 23:24:28 +0200

    Changed Color up a bit (for a better look) & added download complete message

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import tkinter
 import customtkinter
 import pytube
 from pytube import *
+from tkinter import messagebox
+
+def downloadCompleteMessage():
+    downloadComplete = messagebox.showinfo("Download", "Download Complete!")
 
 def startMP3Download():
     try: 
@@ -13,6 +17,7 @@ def startMP3Download():
     except:
         print("YouTube link is invalid")
     print("Download Complete!")
+    downloadCompleteMessage()
     link.delete(0, 'end')
 
 def startVideoDownload():
@@ -24,10 +29,8 @@ def startVideoDownload():
     except:
         print("YouTube link is invalid")
     print("Download Complete!")
+    downloadCompleteMessage()
     link.delete(0, 'end')
-
-downloadAudioThread = threading.Thread(target=startMP3Download)
-downloadVideoThread = threading.Thread(target=startVideoDownload)
 
 #system settings
 customtkinter.set_appearance_mode("dark")
@@ -36,10 +39,10 @@ customtkinter.set_default_color_theme("blue")
 #our app frame
 app = customtkinter.CTk()
 app.geometry("720x480")
-app.title("Youtube Downloader")
+app.title("MyPlayer")
 
 #adding ui elements
-title = customtkinter.CTkLabel(app, text="YouTube Downloader", corner_radius=20, fg_color="darkgrey",text_color="red")
+title = customtkinter.CTkLabel(app, text="MyPlayer", corner_radius=20, fg_color="darkgrey",text_color="#8F0B0B")
 title.pack(padx=10, pady=10)
 
 #link input
@@ -50,8 +53,13 @@ link.pack()
 #download button
 downloadButtons = customtkinter.CTkFrame(app, width=600, height=50)
 downloadButtons.pack(expand=True)
-downloadMp3 = customtkinter.CTkButton(app, text="MP3", command=downloadAudioThread.start)
-downloadVideo = customtkinter.CTkButton(app, text="Video", command=downloadVideoThread.start)
+
+#download buttons initialized
+#multithreading implemented with download functions to prevent application from freezing
+downloadMp3 = customtkinter.CTkButton(app, text="MP3", command=threading.Thread(target=startMP3Download).start)
+downloadVideo = customtkinter.CTkButton(app, text="Video", command=threading.Thread(target=startVideoDownload).start)
+
+#buttons packed in GUI
 downloadMp3.pack(in_=downloadButtons ,padx=30, pady=30, )
 downloadVideo.pack(in_=downloadButtons, padx=30, pady=30)
 
